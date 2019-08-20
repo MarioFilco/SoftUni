bc3887aaba13dac544dddbd76d1b45156cbdf89a
MarioFilco <m_filipov@yahoo.com>
2019-08-20 23:57:36 +0300

    4

--- python_fundamentals_open_courses/02.functions_and_debugging/04.draw_a_filled_square.py
+++ python_fundamentals_open_courses/02.functions_and_debugging/04.draw_a_filled_square.py
@@ -1,14 +1,3 @@
-from itertools import combinations
-from operator import mul
-from functools import reduce
-n = [4, 3, 5]
-k = 2
-for x in combinations(n, k):
-  print(list(x))
-  print(reduce(lambda a,b: a*b,list(x)))
-
-print((list(x)) for x in combinations(n, k))
-
 
 fact = lambda x: 1 if x <= 1 else x * fact(x-1)
 print(fact(10))
