3985130a82297682fd792f379583d6f8756ef3f7
home <m_filipov@yahoo.com>
2019-08-20 21:27:03 +0300

    2

--- python_fundamentals_open_courses/02.functions_and_debugging/04.draw_a_filled_square.py
+++ python_fundamentals_open_courses/02.functions_and_debugging/04.draw_a_filled_square.py
@@ -1,8 +1,21 @@
-print(1234)
-print("".join(map(lambda x: "Happy birthday to ", range(4))))
+from itertools import combinations
+from operator import mul
+n = [4, 3, 5]
+k = 2
+for x in combinations(n, k):
+  print(list(x))
+  print(mul(x))
+print(list(mul(list(x))) for x in combinations(n, k))
 
 
-n = [1, 2, 3, 4, 5, 6]
-d = 5
-print([num for num in n if num % d == 0])
+fact = lambda x: 1 if x <= 1 else x * fact(x-1)
+print(fact(10))
 
+fact = lambda x: x and x * fact(x - 1) or 1
+print(fact(10))
+
+fact = lambda x: 1 if x == 0 else x * fact(x-1)
+print(fact(10))
+
+fact = lambda n: [1, 0][n > 1] or fact(n - 1) * n
+print(fact(10))
--- python_fundamentals_open_courses/test.py
+++ python_fundamentals_open_courses/test.py
@@ -21,3 +21,33 @@ print(fib(10))
 fib = lambda x: list(map(lambda y: int(((1 + sqrt(5)) ** y - (1 - sqrt(5)) ** y) / (2 ** y * sqrt(5))), range(1, x+1)))
 print(fib(100))
 #################
+
+fact = lambda x: 1 if x <= 1 else x * fact(x-1)
+print(fact(4))
+
+fact = lambda x: x and x * fact(x - 1) or 1
+print(fact(10))
+
+fact = lambda x: 1 if x == 0 else x * fact(x-1)
+print(fact(10))
+
+fact = lambda n: [1, 0][n > 1] or fact(n - 1) * n
+print(fact(4))
+
+from functools import reduce
+
+fact = lambda x: reduce(lambda a, b: a*b, range(1, x+1))
+print(fact(4))
+
+from math import factorial
+
+fact = lambda x: factorial(x)
+print(fact(10))
+
+from operator import mul
+
+fact = lambda x: reduce(mul, range(1, x+1))
+print(fact(10))
+
+
+###################
